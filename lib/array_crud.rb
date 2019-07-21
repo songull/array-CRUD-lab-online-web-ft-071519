9d9b560ed3a9e4e0da082557b3196c049ec9dcaf
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-21 04:16:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 music = ["lb", "gc", "m", "op"]
-music.unshift ("ny")
+music.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
