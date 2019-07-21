3ee6ef4692282fcac286ee1b0606284ddd456980
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-21 04:14:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ puts cats.push
 end
 
 def add_element_to_start_of_array(array, element)
-
+music = ["lb", "gc", "m", "op"]
+music.unshift ("ny")
 end
 
 def remove_element_from_end_of_array(array)
